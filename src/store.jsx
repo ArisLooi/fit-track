909b603e5f1c8d10b69e1182650b2ccd485ab923
Aris Looi <arislooi@outlook.com>
2024-12-20 15:07:35 +0800

    remove unused workoutSlice.js, update store.jsx

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../src/features/auth/authSlice';
-import workoutsReducer from '../src/features/workouts/workoutsSlice';
 import timerReducer from '../src/features/timer/timerSlice';
 import exerciseHistoryReducer from '../src/features/exerciseHistory/exerciseHistorySlice';
 import userReducer from '../src/features/user/userSlice';
@@ -9,7 +8,7 @@ import userReducer from '../src/features/user/userSlice';
 export const store = configureStore({
     reducer: {
         auth: authReducer, // Reducer for authentication state
-        workouts: workoutsReducer, // Reducer for workouts state
+
         timer: timerReducer, // Reducer for timer state
         exerciseHistory: exerciseHistoryReducer, // Reducer for exercise history state
         user: userReducer, // Reducer for user data state
